2ca3dd54f5b02079df33f2b2226497500e720a95
Luong Duc Trong <102021631+trongldwork@users.noreply.github.com>
2024-12-25 14:27:18 +0700

    Update evaluation.py

--- evaluation_matrics/evaluation.py
+++ evaluation_matrics/evaluation.py
@@ -2,7 +2,7 @@ import sys
 import os
 import cv2
 import numpy as np
-
+import argparse
 import math
 from PIL import Image
 from datetime import datetime
@@ -14,23 +14,22 @@ from fid import calculate_fid, calculate_fid_yyx
 from cdc import calculate_cdc, calculate_cdc_yyx 
 
 
-
 # D. Hasler and S. E. Suesstrunk, “Measuring colorfulness in natural images,” in Human Vision and Electronic Imaging VIII, B. E. Rogowitz and T. N. Pappas, Eds., vol. 5007, International Society for Optics and Photonics. SPIE, 2003, pp. 87 – 95. [Online]. Available: https://doi.org/10.1117/12.477378 7
 def image_colorfulness(image):
-	# split the image into its respective RGB components
-	(B, G, R) = cv2.split(image.astype("float"))
-	# compute rg = R - G
-	rg = np.absolute(R - G)
-	# compute yb = 0.5 * (R + G) - B
-	yb = np.absolute(0.5 * (R + G) - B)
-	# compute the mean and standard deviation of both `rg` and `yb`
-	(rbMean, rbStd) = (np.mean(rg), np.std(rg))
-	(ybMean, ybStd) = (np.mean(yb), np.std(yb))
-	# combine the mean and standard deviations
-	stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
-	meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
-	# derive the "colorfulness" metric and return it
-	return stdRoot + (0.3 * meanRoot)
+    # split the image into its respective RGB components
+    (B, G, R) = cv2.split(image.astype("float"))
+    # compute rg = R - G
+    rg = np.absolute(R - G)
+    # compute yb = 0.5 * (R + G) - B
+    yb = np.absolute(0.5 * (R + G) - B)
+    # compute the mean and standard deviation of both `rg` and `yb`
+    (rbMean, rbStd) = (np.mean(rg), np.std(rg))
+    (ybMean, ybStd) = (np.mean(yb), np.std(yb))
+    # combine the mean and standard deviations
+    stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
+    meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
+    # derive the "colorfulness" metric and return it
+    return stdRoot + (0.3 * meanRoot)
 
 def calculate_psnr(img1, img2):
     mse_value = ((img1 - img2)**2).mean()
@@ -93,46 +92,53 @@ def calculate_psnr_for_folder(gt_folder, result_folder, loss_fn_alex, flag_CalPS
     avg_lpips = np.mean(lpips)
     return avg_psnr, avg_ssim, avg_colorfulness, avg_lpips
 
-flag_CalFid = False
-flag_CalCDC = False
-flag_CalPSNR = True # True False
-flag_CalSSIM = True
-flag_CalLPIPS = False
-flag_CalColorfulness = False
 
-# Input folder only for an example here, please change the path to your own folder
-GT_dataset_folder = './input/'
-real_save_folder = './input/'
+def main():
+    parser = argparse.ArgumentParser(description="Calculate image quality metrics.")
+    parser.add_argument("--gt_folder", type=str, required=True, help="Path to the ground truth folder.")
+    parser.add_argument("--result_folder", type=str, required=True, help="Path to the result folder.")
+    parser.add_argument("--fid", action="store_true", help="Calculate FID.")
+    parser.add_argument("--cdc", action="store_true", help="Calculate CDC.")
+    parser.add_argument("--psnr", action="store_true", help="Calculate PSNR.")
+    parser.add_argument("--ssim", action="store_true", help="Calculate SSIM.")
+    parser.add_argument("--lpips", action="store_true", help="Calculate LPIPS.")
+    parser.add_argument("--colorfulness", action="store_true", help="Calculate Colorfulness.")
 
-if flag_CalFid:
-    print('Calculating FID for %s'%(real_save_folder))
-    fid = calculate_fid_yyx(real_save_folder, GT_dataset_folder, 100)
-    print('%s FID: %s'%(real_save_folder, fid))
+    args = parser.parse_args()
 
-if flag_CalCDC:
-    print('Calculating CDC for %s'%(real_save_folder))
-    cdc = calculate_cdc_yyx(real_save_folder)
-    print('%s CDC: %s'%(real_save_folder, cdc))
-
-if flag_CalPSNR or flag_CalSSIM or flag_CalLPIPS or flag_CalColorfulness:
-    if flag_CalLPIPS:
-        loss_fn_alex = lpips.LPIPS(net='alex', verbose=False) # best forward scores
-    else:
-        loss_fn_alex = None
-    print('Calculating PSNR SSIM lpips colorfulness for %s'%(real_save_folder))
+    flag_CalFid = args.fid
+    flag_CalCDC = args.cdc
+    flag_CalPSNR = args.psnr
+    flag_CalSSIM = args.ssim
+    flag_CalLPIPS = args.lpips
+    flag_CalColorfulness = args.colorfulness
     
-    psnr, ssim, colorfulness, lpips_score = calculate_psnr_for_folder(GT_dataset_folder, real_save_folder, loss_fn_alex, flag_CalPSNR, flag_CalSSIM, flag_CalLPIPS, flag_CalColorfulness)
-    print('%s PSNR: %s SSIM %s lpips %s colorfulness %s'%(real_save_folder, psnr, ssim, lpips_score, colorfulness))
+    GT_dataset_folder = args.gt_folder
+    real_save_folder = args.result_folder
 
-# if flag_CalPSNR:
-#     print('Calculating PSNR for %s %s'%(dataset, method_name))
-#     psnr = calculate_psnr_for_folder(GT_dataset_folder, real_save_folder)
-#     print('%s %s PSNR: %s'%(dataset, method_name, cdc))
+    if flag_CalFid:
+        print('Calculating FID for %s'%(real_save_folder))
+        fid = calculate_fid_yyx(real_save_folder, GT_dataset_folder, 100)
+        print('%s FID: %s'%(real_save_folder, fid))
 
-if flag_CalPSNR and flag_CalCDC and flag_CalFid:
-    print('*********************************************************************************************************')
-    print('%s FID: %s CDC: %s PSNR: %s SSIM %s lpips %s colorfulness %s'%(real_save_folder, fid, cdc, psnr, ssim, lpips_score, colorfulness))
+    if flag_CalCDC:
+        print('Calculating CDC for %s'%(real_save_folder))
+        cdc = calculate_cdc_yyx(real_save_folder)
+        print('%s CDC: %s'%(real_save_folder, cdc))
 
+    if flag_CalPSNR or flag_CalSSIM or flag_CalLPIPS or flag_CalColorfulness:
+        if flag_CalLPIPS:
+            loss_fn_alex = lpips.LPIPS(net='alex', verbose=False)
+        else:
+            loss_fn_alex = None
+        print('Calculating PSNR SSIM lpips colorfulness for %s'%(real_save_folder))
+    
+        psnr, ssim, colorfulness, lpips_score = calculate_psnr_for_folder(GT_dataset_folder, real_save_folder, loss_fn_alex, flag_CalPSNR, flag_CalSSIM, flag_CalLPIPS, flag_CalColorfulness)
+        print('%s PSNR: %s SSIM %s lpips %s colorfulness %s'%(real_save_folder, psnr, ssim, lpips_score, colorfulness))
 
+    if flag_CalPSNR and flag_CalCDC and flag_CalFid:
+        print('*********************************************************************************************************')
+        print('%s FID: %s CDC: %s PSNR: %s SSIM %s lpips %s colorfulness %s'%(real_save_folder, fid, cdc, psnr, ssim, lpips_score, colorfulness))
 
-
+if __name__ == "__main__":
+    main()
